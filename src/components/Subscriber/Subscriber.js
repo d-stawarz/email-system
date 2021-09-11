a15129bba9530e8ee877801b9fd216a55393ab1b
Barbara Kałuża <basianiemczyk1212@gmail.com>
2021-09-11 12:27:40 +0200

    create feature/add-new-subscriber

--- src/components/Subscriber/Subscriber.js
+++ src/components/Subscriber/Subscriber.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
+import "./styles.scss";
 
 const Subscriber = () => {
-    return (<div>test</div>);
+  const { register } = useForm();
+  return (
+    <form className="form">
+      <label>First name</label>
+      <input
+        className="form__input"
+        {...register("firstName")}
+        autoComplete="off"
+      ></input>
+
+      <label>E-mail login</label>
+      <input
+        className="form__input"
+        {...register("E-mailLogin")}
+        autoComplete="off"
+      ></input>
+
+      <button className="form__button" type="submit">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default Subscriber;
