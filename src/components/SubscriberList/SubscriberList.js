bed0664c36414cb2b137f9b8156d20fc9af92ac3
Dominik Stawarz <dominik.stawarz@goodc.pl>
2021-09-11 13:01:29 +0200

    Revert "feature/newSubscribersList"
    This reverts commit c53f7ffd24ecfcf0e97e1f4a0fe4426a8b41f5f8.

--- src/components/SubscriberList/SubscriberList.js
+++ src/components/SubscriberList/SubscriberList.js
@@ -1,30 +1,7 @@
 import React from 'react';
-import "./styles/subscriberList.scss";
 
-const SubscriberList = (props) => {
-    const { className } = props;
-
-    return (
-        <div className={`${className}`}>
-            <div className={`${className}__label`}>
-                <>Subscribers</>
-            </div>
-            <table className={`${className}--table`}>
-                <tr className={`${className}--table__tr`}>
-                    <th>Email</th>
-                    <th>Name</th>
-                    <th>Created</th>
-                </tr>
-                {[].map(user =>
-                    <tr>
-                        <td className={`${className}--table__td`}>{user.email}</td>
-                        <td className={`${className}--table__td`}>{user.name}</td>
-                        <td className={`${className}--table__td`}>{user.created}</td>
-                    </tr>
-                )}
-            </table>
-        </div>
-    );
+const SubscriberList = () => {
+    return (<div>test</div>);
 };
 
 export default SubscriberList;
