f1d74ace9c6d8c4a35c8bb4d904ee6b240d9b105
Dominik Stawarz <dominik.stawarz@goodc.pl>
2021-09-11 11:27:59 +0200

    Base structure, install node-sass and react-ruter-dom packages

---
+++ src/components/Page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page = ({Component}) => {
+    return (
+            <Component/>
+    )
+};
+
+export default Page;
---
+++ src/components/SubscriberList/SubscriberList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SubscriberList = () => {
+    return (<div>test</div>);
+};
+
+export default SubscriberList;
